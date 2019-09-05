209fc265701a355192d19e245adeb4c09b49337e
Joe Untrecht <799783@seq.org>
2019-09-04 20:31:06 -0700

    lab 903

--- Lab 903/ball.js
+++ Lab 903/ball.js
@@ -5,18 +5,21 @@ class Ball{
     this.loc= createVector(x,y);
     this.vel = createVector(dx,dy);
     this.clr = color(random(255),random(255),random(255));
-    this.acc=createVector(0,.24);
-    this.w=15;
+    this.acc=createVector(0,0);
     this.id=id;
     if (this.id<0){
-      this.w=50
+      this.w=50;
+    }
+    else{
+      this.w=15
     }
 
   }
+
   run(){
+    this.render();
     this.checkEdges();
     this.update();
-    this.render();
   }
   checkEdges(){
     if(this.loc.x<0){
@@ -30,32 +33,32 @@ class Ball{
     }
     if(this.loc.y>height){
       this.vel.y = -this.vel.y;
-      this.loc.y=height-2;
     }
   }
 
   update(){
-    this.loc.add(this.vel);
-    this.vel.add(this.acc);
-    var distTomainBall;
-    if(this.id>=0){
-      distTomainBall = this.loc.dist(mainBall.loc);
-      if (distTomainBall<250){
+    var distToMainBall
+    if(this.id >= 0){
+      distToMainBall = this.loc.dist(mainBall.loc);
+      if (distToMainBall<250){
         this.acc=p5.Vector.sub(mainBall.loc,this.loc);
         this.acc.normalize();
-        this.acc.mult(0.1);
+        this.acc.mult(.07);
       }
-      if(distTomainBall<150){
+      if(distToMainBall<150){
         this.acc=p5.Vector.sub(this.loc,mainBall.loc);
         this.acc.normalize();
         this.acc.mult(0.5);
       }
     }
+    this.vel.limit(10);
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
   }
   render(){
       fill(this.clr);
       ellipse(this.loc.x, this.loc.y, this.w, this.w);
     }
 
-  }
+}
 //end ball class
--- Lab 903/sketch.js
+++ Lab 903/sketch.js
@@ -2,26 +2,25 @@
 // 	8/28 Vector
 //  This is a comment
 //  The setup function function is called once when your program begins
-var balls= [];
-var mainBall;
+var balls= []
+var mainBall=[]
 function setup() {
   // put setup code here
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
-  loadBalls(50);
+  background(20, 20, 20);
+  loadBalls(500);
 }
 
 function draw() {
-  background(5,5,5);
+  background(5,5,5,50);
   runBalls();
 }
 
 function loadBalls(n){
   mainBall = new Ball(width/2,height/2,random(-1,1),random(-1,1),-1);
   for(var i=0; i<n; i++){
-    balls[i]= new Ball(random(0,800), random(0,800),random(-8,8),random(-8,8),i);
+    balls[i]= new Ball(random(width), random(height),random(-2,2),random(-2,2),i);
   }
 }
 
