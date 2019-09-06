83fd5a77f7318199590510c136039b71264a4cef
Joe Untrecht <799783@seq.org>
2019-09-06 08:47:59 -0700

    lab 906

---
+++ Lab906/ship.js
@@ -0,0 +1,39 @@
+//Joe Untrecht
+//828 Vector
+class Ship{
+  constructor(x,y,dx,dy,id){
+    this.loc= createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.angle=0;
+    this.clr=color(random(255),random(255),random(255));
+
+  }
+
+  run(){
+    this.render();
+    this.checkEdges();
+    this.update();
+  }
+  checkEdges(){
+    if(this.loc.x<0 ) this.loc.x=width;
+    if(this.loc.x>width ) this.loc.x=0;
+    if(this.loc.y<0 ) this.loc.y=height;
+    if(this.loc.y>height ) this.loc.y=0;
+  }
+
+  update(){
+    this.vel.add(this.acc);
+    this.vel.limit(2);
+    this.loc.add(this.vel);
+  }
+  render(){
+      fill(this.clr);
+      this.angle=this.angle+.1;
+      push();
+      rotate(this.angle);
+      triangle(-5,8,5,8,0,-8);
+      pop();
+    }
+
+}
+//end ball class
---
+++ Lab906/sketch.js
@@ -0,0 +1,34 @@
+//  Joe Untrecht
+// 	8/28 Vector
+//  This is a comment
+//  The setup function function is called once when your program begins
+var balls= []
+var attractor, repeller;
+function setup() {
+  // put setup code here
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  loadObjects(12);
+}
+
+function draw() {
+  background(5,5,5,50);
+  runBalls();
+}
+
+function loadObjects(n){
+  attractor= new Ball(width/2,height/2,random(-1,1),random(-1,1),-1);
+  repeller=new Ball(width/2,height/2,random(-1,1),random(-1,1),-2);
+    for(var i=0; i<n, i++){
+      ships[i] = new Ship(random(width), random(height), random(-2,2), random(-2,2), i);
+    }
+}
+
+function runBalls(){
+  attractor.run();
+  repeller.run();
+  for(var i=0; i<ships.length; i++){
+    ships[i].run();
+  }
+}
