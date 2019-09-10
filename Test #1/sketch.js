b2f54300c6800fd9aecbff06a39ef00428a02350
Joe Untrecht <799783@seq.org>
2019-09-09 20:04:04 -0700

    test

---
+++ Test #1/sketch.js
@@ -0,0 +1,14 @@
+//  Joe Untrecht
+// 	8/28 Vector
+//  This is a comment
+//  The setup function function is called once when your program begins
+var num=1
+if (num<4){
+  console.log("Peace");
+}
+console.log("War");
+
+for(var i=14;i>8; i--){
+  i=i-4
+  console.log(i);
+}
