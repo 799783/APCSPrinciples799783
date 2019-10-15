17f2ae2fa5e4a596a75f5978cb118fda7568a81b
Joe Untrecht <juntrecht@WS-ACADBUS.EDU>
2019-10-15 08:49:51 -0700

    sort

---
+++ lab 1015 Sorting/sketch.js
@@ -0,0 +1,20 @@
+//  Joe Untrecht
+// 	1009
+//  This is a comment
+//  The setup function function is called once when your program begins
+//bubble sort
+var list=[];
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(235);
+  
+  }
+}
+}
+function swap(list,a,b){
+  var temp=list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
+console.log(list);
