e2224faa7e641f1591d848791ae946e568dc1702
Joe Untrecht <799783@seq.org>
2019-10-15 22:15:13 -0700

    sort

--- lab 1015 Sorting/sketch.js
+++ lab 1015 Sorting/sketch.js
@@ -8,10 +8,10 @@ function setup(){
   var cnv=createCanvas(800,800);
   cnv.position((windowWidth-width)/2,30);
   background(235);
-  
+  for (var i=0; i<10; i++){
+    list[i]= random(10);
   }
-}
-}
+  }
 function swap(list,a,b){
   var temp=list[a];
   list[a]=list[b];
