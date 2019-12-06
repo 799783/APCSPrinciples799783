ebed7d3e2dd8fe47735761516317f239a4313b5c
Joe Untrecht <juntrecht@WS-ACADBUS.EDU>
2019-12-06 07:49:09 -0800

    final project

---
+++ Lab Final Project/planet.js
@@ -0,0 +1,55 @@
+//Joe Untrecht
+//12/03 Vector
+//ball.js
+class Planet{
+  constructor(x,y,dx,dy){
+    this.loc= createVector(x,y);
+    this.vel = createVector(dx,dy);
+    this.clr = color(random(255),random(255),random(255));
+    this.acc=createVector(0,0);
+    this.w=50;
+
+  }
+
+  run(){
+    this.render();
+    this.checkEdges();
+    this.update();
+  }
+  checkEdges(){
+    //teleports planet if it hits any edges
+    if(this.loc.x>800){
+      this.loc.x=0;
+    }
+    if(this.loc.x<0){
+      this.loc.x=800;
+    }
+    if(this.loc.y>800){
+      this.loc.y=0;
+    }
+    if(this.loc.y<0){
+      this.loc.y=800;
+    }
+
+  }
+
+  update(){
+    //if the ship gets close to planet
+    if(dist<100){
+      //planets changes location and velocity randomly
+      this.loc=createVector(random(width),random(height));
+      this.vel=createVector(random(-3,3),random(-3,3));
+    }
+    //planet moves
+    this.vel.limit(3);
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
+  }
+  render(){
+    //renders planet
+      fill(this.clr);
+      ellipse(this.loc.x, this.loc.y, this.w, this.w);
+    }
+
+}
+//end ball class
---
+++ Lab Final Project/sketch.js
@@ -0,0 +1,38 @@
+//  Joe Untrecht
+// 12/03 Vector
+//  This is a comment
+//  The setup function function is called once when your program begins
+//sketch
+planet=[];
+ships=[];
+function setup() {
+  // put setup code here
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20, 20, 20);
+  loadObjects(50);
+}
+
+function draw() {
+  background(5,5,5,50);
+  //runs ship and planet
+  runObjects();
+}
+
+function loadObjects(n){
+  //loads planet
+    planet=  new Planet(width/2,height/2,random(-3,3),random(-3,3));
+
+  //loads ship
+  for (var i=0; i>10; i++){
+    ship[i]=new Ship(random(width), random(height), random(-2,2), random(-2,2));
+  }
+
+}
+
+function runObjects(){
+  //runs planet
+  planet.run();
+  //runs ship
+  ship.run();
+}
